1f43e78fe040aa13454038d7ed26e4ca81cf91a3
smvelapy <168081317+smvelapy@users.noreply.github.com>
2024-04-09 19:35:56 +0300

    Add an analog sensor reading feature

--- main.c
+++ main.c
@@ -1,38 +1,37 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
 
 
 // #include "src/calc.h"
 
-// #define LED_PIN = 15;
+#define TEMP_ADC_PIN 26
+
 int main() {
-    const unsigned int LED_PIN = 16;//15;
     stdio_init_all();
+
     printf("Hello world\n");
+
+    adc_init();
+    adc_gpio_init(TEMP_ADC_PIN);
+
+
     // gpio_init(PICO_DEFAULT_LED_PIN);
     // gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
     // gpio_init(LED_PIN);
     // gpio_set_dir(LED_PIN, GPIO_OUT);
     unsigned int uptime = 0;
+
+    adc_select_input(0);
     while(true) {
-        printf("Uptime: %d\n", uptime);
-        // if (uptime < 10) {
-        //     gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        //     sleep_ms(500);
-        //     gpio_put(PICO_DEFAULT_LED_PIN, 0);
-        //     sleep_ms(500);
-        // }else if (uptime == 20) {
-        //     printf("Turning LED on.\n");
-        //     gpio_put(LED_PIN, 1);
-        //     // sleep_ms(500);
-        //     // gpio_put(LED_PIN, 0);
-        //     // sleep_ms(500);
-        // }else {
-        //     // printf("Ending program.\n");
-        //     sleep_ms(1000);
-        //     // break;
-        // }
+        // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
+        const float conversion_factor = 3.3f / (1 << 12);   //8*10^-4 3.3 / 4096
+        uint16_t result = adc_read(); // 0x430 = 1072
+        float c_per_mv = 19.5f;
+        float tmp = ((result * conversion_factor) * 1000-400) / c_per_mv;
+        printf("Temp: %f'c Raw value: %d, voltage: %f V\n", tmp, result, result * conversion_factor);
+        // printf("Uptime: %d\n", uptime);
         uptime++;
         sleep_ms(1000);
     }
